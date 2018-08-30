37c94d1f85a73c9c33d4a71d2dde4433bca62501
6e38 <6e38@floorsix.com>
2018-08-30 06:47:07 -0600

    Adding clock

--- src/dashboard/clock/ClockPanel.java
+++ src/dashboard/clock/ClockPanel.java
@@ -6,35 +6,74 @@ import java.awt.Dimension;
 import java.awt.event.ComponentEvent;
 import java.awt.event.ComponentListener;
 import java.awt.Font;
+import java.awt.FontFormatException;
+import java.awt.FontMetrics;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Point;
+import java.awt.RenderingHints;
+import java.io.InputStream;
+import java.io.IOException;
+import java.lang.StringBuilder;
+import java.util.Calendar;
 import javax.swing.JPanel;
 
 public class ClockPanel extends JPanel implements ComponentListener
 {
+  private static final String[] DaysOfWeek = {
+    "", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
+  };
+  private static final String[] Months = {
+    "January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"
+  };
   private Font clockFont;
-  private int paints;
+  private Font dateFont;
+  private Dimension preferredSize;
+  private boolean isSized;
+  private String timeString;
+  private String dateString;
+  private Point timePoint;
+  private Point datePoint;
 
   public ClockPanel()
   {
     super();
 
-    clockFont = new Font(Font.SANS_SERIF, Font.PLAIN, 80);
+    try
+    {
+      InputStream stream = getClass().getResourceAsStream("/fonts/LANEUP__.ttf");
+      Font font = Font.createFont(Font.TRUETYPE_FONT, stream);
+      clockFont = font.deriveFont(100f);
+      dateFont = font.deriveFont(30f);
+    }
+    catch (FontFormatException fontFormatException)
+    {
+      System.out.println("Failed to load font");
+      clockFont = new Font(Font.SANS_SERIF, Font.PLAIN, 80);
+      dateFont = new Font(Font.SANS_SERIF, Font.PLAIN, 20);
+    }
+    catch (IOException ioException)
+    {
+      System.out.println("IO failure while reading font");
+      clockFont = new Font(Font.SANS_SERIF, Font.PLAIN, 80);
+      dateFont = new Font(Font.SANS_SERIF, Font.PLAIN, 20);
+    }
+
+    preferredSize = new Dimension(400, 65535);
+
+    isSized = false;
+
+    timeString = "8:00";
+    dateString = "Monday, August 24, 2018";
 
     addComponentListener(this);
-    paints = 0;
   }
 
   @Override
   protected void paintComponent(Graphics g)
   {
     super.paintComponent(g);
-
-    if (paints < 10)
-    {
-      ++paints;
-      System.out.println("paintComponent");
-    }
 
     Graphics2D g2d = (Graphics2D) g.create();
     update();
@@ -44,6 +83,14 @@ public class ClockPanel extends JPanel implements ComponentListener
 
   private void update()
   {
+    Calendar c = Calendar.getInstance();
+
+    timeString = String.format("%d:%02d", c.get(Calendar.HOUR_OF_DAY), c.get(Calendar.MINUTE));
+    dateString = String.format("%s %d %s %d",
+        DaysOfWeek[c.get(Calendar.DAY_OF_WEEK)],
+        c.get(Calendar.DAY_OF_MONTH),
+        Months[c.get(Calendar.MONTH)],
+        c.get(Calendar.YEAR));
   }
 
   private void draw(Graphics2D g)
@@ -51,110 +98,85 @@ public class ClockPanel extends JPanel implements ComponentListener
     g.setColor(Color.BLACK);
     g.fillRect(0, 0, getWidth(), getHeight());
 
-    g.setColor(Color.GREEN);
-    g.setFont(clockFont);
-    g.drawString("8:00", 40, 80);
-  }
+    if (isSized)
+    {
+      g.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_GASP);
+      g.setColor(Color.GREEN);
 
-  /*
-  @Override
-  public void setSize(Dimension d)
-  {
-    System.out.println(String.format("setSize(%f,%f)", d.getWidth(), d.getHeight()));
-  }
+      FontMetrics metrics;
+      metrics = g.getFontMetrics(clockFont);
+      int y = metrics.getAscent();
+      int w = metrics.stringWidth(timeString);
+      int x = getWidth() / 2 - w / 2;
 
-  @Override
-  public Dimension getSize()
-  {
-    System.out.println("getSize()");
-    return new Dimension(100, 100);
+      g.setFont(clockFont);
+      g.drawString(timeString, x, y);
+
+      metrics = g.getFontMetrics(dateFont);
+      y += metrics.getAscent();
+      w = metrics.stringWidth(dateString);
+      x = getWidth() / 2 - w / 2;
+
+      g.setFont(dateFont);
+      g.drawString(dateString, x, y);
+    }
   }
-  */
 
   @Override
   public boolean isMaximumSizeSet()
   {
-    System.out.println("isMaximumSizeSet()");
     return true;
   }
 
   @Override
   public boolean isMinimumSizeSet()
   {
-    System.out.println("isMinimumSizeSet()");
     return true;
   }
 
   @Override
   public boolean isPreferredSizeSet()
   {
-    System.out.println("isPreferredSizeSet()");
     return true;
   }
 
   @Override
   public Dimension getMaximumSize()
   {
-    System.out.println("getMaximumSize()");
-    return new Dimension(300, 65635);
+    return preferredSize;
   }
 
   @Override
   public Dimension getMinimumSize()
   {
-    System.out.println("getMinimumSize()");
-    return new Dimension(300, 65635);
+    return preferredSize;
   }
 
   @Override
   public Dimension getPreferredSize()
   {
-    System.out.println("getPreferredSize()");
-    return new Dimension(300, 65535);
+    return preferredSize;
   }
-
-  /*
-  @Override
-  public void setMaximumSize(Dimension d)
-  {
-    System.out.println(String.format("setMaximumSize(%f,%f)", d.getWidth(), d.getHeight()));
-  }
-
-  @Override
-  public void setMinimumSize(Dimension d)
-  {
-    System.out.println(String.format("setMinimumSize(%f,%f)", d.getWidth(), d.getHeight()));
-  }
-
-  @Override
-  public void setPreferredSize(Dimension d)
-  {
-    System.out.println(String.format("setPreferredSize(%f,%f)", d.getWidth(), d.getHeight()));
-  }
-  */
 
   @Override
   public void componentHidden(ComponentEvent e)
   {
-    System.out.println("componentHidden");
   }
 
   @Override
   public void componentMoved(ComponentEvent e)
   {
-    System.out.println("componentMoved");
   }
 
   @Override
   public void componentResized(ComponentEvent e)
   {
-    System.out.println(String.format("componentResized %d,%d", getWidth(), getHeight()));
+    isSized = true;
   }
 
   @Override
   public void componentShown(ComponentEvent e)
   {
-    System.out.println("componentShown");
   }
 }
 
