b2b3892bc7954df8ba654736b2fc83a7130769d6
6e38 <6e38@floorsix.com>
2018-09-05 06:36:16 -0600

    Refactor server

--- src/dashboard/server/Server.java
+++ src/dashboard/server/Server.java
@@ -7,6 +7,7 @@ package dashboard.server;
 import dashboard.Data;
 import java.io.InputStream;
 import java.io.IOException;
+import java.net.ServerSocket;
 import java.net.Socket;
 import javax.net.ssl.SSLServerSocket;
 import javax.net.ssl.SSLServerSocketFactory;
@@ -26,44 +27,55 @@ public class Server implements Runnable
 
     try
     {
-      SSLServerSocket sock = (SSLServerSocket)factory.createServerSocket(5005);
+      SSLServerSocket server = (SSLServerSocket)factory.createServerSocket(5005);
 
-      sock.setEnabledCipherSuites(factory.getSupportedCipherSuites());
+      server.setEnabledCipherSuites(factory.getSupportedCipherSuites());
 
-      try
-      {
-        Socket s = sock.accept();
-
-        try
-        {
-          InputStream in = s.getInputStream();
-
-          try
-          {
-            while (true)
-            {
-              int c = in.read();
-              System.out.print((char)c);
-            }
-          }
-          catch (IOException e1)
-          {
-            System.out.println("Error reading byte from client");
-          }
-        }
-        catch (IOException e2)
-        {
-          System.out.println("Error reading data from client");
-        }
-      }
-      catch (IOException e3)
-      {
-        System.out.println("Error waiting for client");
-      }
+      accept(server);
     }
     catch (IOException e4)
     {
       System.out.println("Failed to create server socket");
+    }
+  }
+
+  private void accept(ServerSocket server)
+  {
+    try
+    {
+      getInput(server.accept());
+    }
+    catch (IOException e)
+    {
+      System.out.println("Error waiting for client");
+    }
+  }
+
+  private void getInput(Socket sock)
+  {
+    try
+    {
+      read(sock.getInputStream());
+    }
+    catch (IOException e)
+    {
+      System.out.println("Error reading data from client");
+    }
+  }
+
+  private void read(InputStream in)
+  {
+    try
+    {
+      while (true)
+      {
+        int c = in.read();
+        System.out.print((char)c);
+      }
+    }
+    catch (IOException e)
+    {
+      System.out.println("Error reading byte from client");
     }
   }
 
